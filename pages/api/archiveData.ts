6283140ad61fc5d6a5b13704101a80655b4a53a0
younha-kim <rladudtmd727@gmail.com>
2023-03-27 18:10:31 +0900

    fix layout to deploy

--- pages/api/archiveData.ts
+++ pages/api/archiveData.ts
@@ -2,10 +2,6 @@
 import { MongoClient } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// type Data = {
-//   name: string
-// }
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
