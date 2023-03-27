a8f419049f7daa5c031e28a2b6b3adfb0118087a
younha-kim <rladudtmd727@gmail.com>
2023-03-27 18:24:51 +0900

    fix layout to deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '@/components/layout/layout'
+import Layout from '@/components/layout/Layout'
 // import MainNavigation from '@/components/navBar/MainNavigation'
 
 export default function App({ Component, pageProps }: AppProps) {
